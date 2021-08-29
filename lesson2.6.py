3e2ae2022b32bd8c3825a2efc965ca5dfc80b9e7
Vitaly <vit050587@mail.ru>
2021-08-29 15:34:36 +0300

    Добавлено решение ко 2-ому уроку 6-ая задача.

--- lesson2.6.py
+++ lesson2.6.py
@@ -22,3 +22,23 @@
 # “количество”: [5, 2, 7],
 # “ед”: [“шт.”]
 # }
+
+goods = []
+features = {'название': '', 'количество': '', 'цена': '', 'единица измерения': ''}
+analitics = {'название': [], 'количество': [], 'цена': [], 'единица измерения': []}
+num = 0
+while True:
+    if input('Для выхода из программы нажмите "Q", для продолжения "Enter": ').upper() == 'Q':
+        break
+    num += 1
+    f_copy = features.copy()
+    for f in features:
+        pro = input(f'Введите значение свойства "{f}": ')
+        f_copy[f] = int(pro) if f == 'цена' or f == 'количество' else pro
+        analitics[f].append(f_copy[f])
+    goods.append((num, f_copy))
+    print(f'\nСтруктура товаров\n{goods}')
+    print(f'\n Текущаа аналитика по товарам: \n {"*" * 30}')
+    for key, value in analitics.items():
+        print(f'{key:>30}: {value}')
+    print("*" * 30)
