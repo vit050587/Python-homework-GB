c2b56741906e03460b4a6c1fb5f5b76142997911
Vitaly <vit050587@mail.ru>
2021-08-29 13:13:26 +0300

    Дописаны Решения ДЗ урока 2 (2) добавлены варианты решения

--- lesson2.2.py
+++ lesson2.2.py
@@ -22,3 +22,21 @@ my_list = list(input("Enter the number without space - "))
 for i in range(1, len(my_list), 2):
     my_list[i - 1], my_list[i] = my_list[i], my_list[i - 1]
 print(my_list)
+
+# ----------------------------------------------------------------------
+
+my_list = input('Ведите числа списка через пробел - ').split()
+print('Введенный список: ', my_list)
+
+idx = len(my_list) if len(my_list) % 2 == 0 else len(my_list) - 1
+
+my_list[:idx:2], my_list[1:idx:2] = my_list[1:idx:2], my_list[:idx:2]
+print('Измененный список: ', my_list)
+
+#-----------------------------------------------------------------------
+
+user_list = input("Enter the number without space - ").split()
+for i in range(1, len(user_list), 2):
+    user_list.insert(i - 1, user_list.pop(i))
+
+print(user_list)
