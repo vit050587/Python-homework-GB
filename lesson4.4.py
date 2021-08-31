176fb52337489ecdd0db6120ad41e17f0efb0088
Vitaly <vit050587@mail.ru>
2021-08-31 17:27:26 +0300

    ДЗ урока 4 (3, 4)

--- lesson4.4.py
+++ lesson4.4.py
@@ -4,3 +4,14 @@
 # Для выполнения задания обязательно использовать генератор.
 #Пример исходного списка: [2, 2, 2, 7, 23, 1, 44, 44, 3, 2, 10, 7, 4, 11].
 #Результат: [23, 1, 3, 10, 4, 11]
+
+
+import random
+list_1 = random.sample(range(1, 11), 10)
+list_2 = random.sample(range(1, 6), 5)
+spisok = list_1 + list_2
+
+print(spisok)
+print("*" * 50)
+result = [el for el in spisok if spisok.count(el) == 1]
+print(result)
