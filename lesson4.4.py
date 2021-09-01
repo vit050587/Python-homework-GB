0097b01e5a711ede627a5310f3e1fb3efdf575b8
Vitaly <vit050587@mail.ru>
2021-09-01 17:56:01 +0300

    Добавлены решения GB к уроку 4 (4)

--- lesson4.4.py
+++ lesson4.4.py
@@ -16,3 +16,19 @@ print(spisok)
 print("*" * 50)
 result = [el for el in spisok if spisok.count(el) == 1]
 print(result)
+
+#-------------решения GB----------------------
+
+
+
+from random import randint
+
+my_list = [randint(-10, 10) for _ in range(20)]
+uniq_list = [el for el in my_list if my_list.count(el) == 1]
+print(f"Source list\n{my_list}\nNo repetition list\n{uniq_list}")
+
+
+#  ------------------------------------------- вариант решения ---------------------------------------------------------
+
+
+print(a := [randint(0, 9) for _ in range(20)], [i for i in a if a.count(i) == 1], sep="\n")
