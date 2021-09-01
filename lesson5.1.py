8b1073b57881796342a2ec253d8eca4d11098db8
Vitaly <vit050587@mail.ru>
2021-09-01 23:28:26 +0300

    Добавлено решение к 5 уроку (1)

--- lesson5.1.py
+++ lesson5.1.py
@@ -2,3 +2,16 @@
 # записать в него построчно данные, вводимые пользователем.
 # Об окончании ввода данных свидетельствует пустая строка.
 
+func = open('test.txt', 'w')
+str_text = input('Введите текст: \n')
+while str_text:
+    func.writelines(str_text)
+    str_text = input('Введите текст: \n')
+    if not str_text:
+        break
+
+func.close()
+func = open('test.txt', 'r')
+content = func.readlines()
+print(content)
+func.close()
