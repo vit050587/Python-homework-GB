2fce80c285244900283b7b4354108be8eec76c5a
Vitaly <vit050587@mail.ru>
2021-09-01 18:00:16 +0300

    Добавлены решения GB к уроку 4 (7)

--- lesson4.7.py
+++ lesson4.7.py
@@ -23,3 +23,53 @@ for el in gen:
         n += 1
     else:
         break
+
+#--------------------решения GB--------------------------------
+
+from itertools import count
+from math import factorial
+
+
+def fact_gen():
+    for el in count(1):
+        yield factorial(el)
+
+
+
+x = 0
+for i in fact_gen():
+    if x == 15:
+        break
+    else:
+        x += 1
+        print(f"Factorial {x} = {i}")
+
+
+#  ------------------------------------------- вариант решения ---------------------------------------------------------
+
+
+def fact_gen(number):
+    f_num = 1
+    for i in range(number + 1):
+        yield f'{i}! = {f_num}'
+        f_num *= i + 1
+
+
+for el in fact_gen(int(input('Factorial number: '))):
+    print(el)
+
+#  ------------------------------------------- вариант решения ---------------------------------------------------------
+
+
+from functools import reduce
+
+
+def fact(n):
+    try:
+        yield reduce(lambda x, y: x * y, list(el if el > 0 else 1 for el in range(n + 1)))
+    except TypeError:
+        yield 0
+
+
+for i in fact(0):
+    print(i)
