5440180fcf8da60d8f525cfe35b71f7228e3503a
Vitaly <vit050587@mail.ru>
2021-09-01 17:57:14 +0300

    Добавлены решения GB к уроку 4 (5)

--- lesson4.5.py
+++ lesson4.5.py
@@ -15,3 +15,23 @@ def result(el_p, el):
 
 
 print(reduce(result, spisok))
+
+#-----------------решения GB------------------------------
+
+from functools import reduce
+
+
+def mul_list(el_1, el_2):
+    return el_1 * el_2
+
+
+uniq_list = [el for el in range(100, 1001, 2)]
+print(f"List\n{uniq_list}\nMultiplication of numbers\n{reduce(mul_list, uniq_list)}")
+
+
+#  ------------------------------------------- вариант решения ---------------------------------------------------------
+
+
+from functools import reduce
+
+print(reduce(lambda a, b: a * b, [x for x in range(100, 1001, 2)]))
