ad3da143cf0b95668b704b1a15b937cb328f3f67
Vitaly <vit050587@mail.ru>
2021-09-01 17:54:16 +0300

    Добавлены решения GB к уроку 4 (2)

--- lesson4.2.py
+++ lesson4.2.py
@@ -16,3 +16,20 @@ for el in range(len(spisok) - 1):
         result.append(spisok[el + 1])
 
 print(result)
+
+#--------------решение GB------------------------------
+
+my_list = [15, 16, 2, 3, 1, 7, 5, 4, 10]
+more_then = [my_list[num] for num in range(1, len(my_list)) if my_list[num] > my_list[num - 1]]
+print(more_then)
+
+#  ------------------------------------------- вариант решения ---------------------------------------------------------
+
+from random import randint
+
+
+original_list = [randint(0, 1000) for _ in range(0, randint(2, 20))]
+answer_list = [num for i, num in enumerate(original_list[1:]) if num > original_list[i]]
+
+print(original_list)
+print(answer_list)
