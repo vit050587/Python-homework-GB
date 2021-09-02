8a2a671c8faf3f969c4a5c1da74f12a4c80625ef
Vitaly <vit050587@mail.ru>
2021-09-02 10:02:48 +0300

    Добавлено решение к 5 уроку (2)

--- lesson5.2.py
+++ lesson5.2.py
@@ -1,18 +1,18 @@
 # 2. Создать текстовый файл (не программно), сохранить в нем несколько строк,
 # выполнить подсчет количества строк, количества слов в каждой строке.
 
-file = open('file.txt', 'r')
-content = file.read()
-print(f'Содержимое файла: \n {content}')
-file = open('file.txt', 'r')
-content = file.readlines()
-print(f'Количество строк в файле - {len(content)}')
-file = open('file.txt', 'r')
-content = file.readlines()
-for i in range(len(content)):
-    print(f'Количество символов {i + 1} строки {len(content[i])}')
-file = open('file.txt', 'r')
-content = file.read()
-content = content.split()
-print(f'Количество слов в строках - {len(content)}')
-file.close()
+
+
+def words_func():
+    try:
+        with open('file.txt', 'r', encoding="utf-8") as file:
+            list = file.readlines()
+            print(f'Количество строк в файле {len(list)}')
+            for i in range(len(list)):
+                str_num = list[i].split()
+                print(f'Количество слов в {i + 1}-ой строке: {len(str_num)}')
+    except FileNotFoundError:
+        return 'Файл не найден.'
+
+
+words_func()
