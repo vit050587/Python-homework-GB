81a461c02b52a7f2c1a96d52c1672608bb4e6500
Vitaly <vit050587@mail.ru>
2021-09-07 22:45:14 +0300

    Добавлены задачи к уроку 6 (1, 2)

--- lesson6.3.py
+++ lesson6.3.py
@@ -7,4 +7,27 @@
 # Проверить работу примера на реальных данных (создать экземпляры класса Position, передать данные,
 # проверить значения атрибутов, вызвать методы экземпляров).
 
+class Worker:
 
+    def __init__(self, name, surname, position, wage, bonus):
+        self.name = name
+        self.surname = surname
+        self.position = position
+        self._income = {"wage": wage, "bonus": bonus}
+
+
+class Position(Worker):
+    def __init__(self, name, surname, position, wage, bonus):
+        super().__init__(name, surname, position, wage, bonus)
+
+    def get_full_name(self):
+        return f"{self.name} {self.surname}"
+
+    def get_total_income(self):
+        return f"{sum(self._income.values())}"
+
+
+user = Position("Анатолий", "Сульянов", "BOSS", 50000, 1250)
+print(user.get_full_name())
+print(user.position)
+print(user.get_total_income())
