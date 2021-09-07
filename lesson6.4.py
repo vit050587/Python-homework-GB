b55b4782316bd51571db8a3916b9839a7a1f183e
Vitaly <vit050587@mail.ru>
2021-09-08 00:45:30 +0300

    Добавлены задачи к уроку 6 (4)

--- lesson6.4.py
+++ lesson6.4.py
@@ -10,3 +10,72 @@
 # Выполните доступ к атрибутам, выведите результат. Выполните вызов методов и также покажите результат.
 
 
+from random import choice
+
+
+class Car:
+    direction = ["право", "лево"]
+
+    def __init__(self, speed, color, name, is_police=False):
+        self.name = name
+        self.speed = speed
+        self.color = color
+        self.is_police = is_police
+        print(f'Автомобиль: {name} {color} цвета. Полицейская? - {is_police}!')
+        print(60 * '-')
+
+    def go(self):
+        print(f'Автомобиль {self.name} едет!')
+
+    def stop(self):
+        print(f'Автомобиль {self.name} стоит!')
+
+    def turn(self):
+        print(f'Автомобиль {self.name} повернул в {choice(self.direction)}')
+
+    def show_speed(self):
+        print(f'со скоростью: {self.speed}')
+
+
+class TownCar(Car):
+    def show_speed(self):
+        if self.speed > 60:
+            print(f'\nС превышением скорости на {self.speed - 60} км/ч.')
+        else:
+            print(f'С разрешенной скоростью: {self.speed} км/ч.')
+
+
+class SportCar(Car):
+    def show_speed(self):
+        print(f'\nС максимальной скоростью: {self.speed} км/ч.')
+
+
+class WorkCar(Car):
+    def show_speed(self):
+        if self.speed > 40:
+            print(f'\nС превышением скорости на {self.speed - 40} км/ч.')
+        else:
+            print(f'С разрешенной скоростью:{self.speed} км/ч.')
+
+
+class PoliceCar(Car):
+    def show_speed(self):
+        if self.speed > 60:
+            print(f'\nС превышением скорости на {self.speed - 60} км/ч. - с мигалкой можно!')
+        else:
+            print(f'С разрешенной скоростью:{self.speed} км/ч.')
+
+
+town = TownCar(90, 'черного', 'АУДИ', False)
+sport = SportCar(270, 'красного', 'БМВ', False)
+work = WorkCar(30, 'белого', 'ГРУЗОВИК', False)
+police = PoliceCar(100, 'синего', 'Жигули', True)
+
+list_of_cars = [police, work, sport, town]
+
+for i in list_of_cars:
+    i.go()
+    print(i.show_speed())
+    i.turn()
+    i.stop()
+    print(60 * '-')
