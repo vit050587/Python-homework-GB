7f6a2d14eb28142aa7a1530afb57e66dd87df214
Vitaly <vit050587@mail.ru>
2021-09-08 00:52:34 +0300

    Добавлены задачи к уроку 6 (5)

--- lesson6.5.py
+++ lesson6.5.py
@@ -6,3 +6,32 @@
 # Создать экземпляры классов и проверить, что выведет описанный метод для каждого экземпляра.
 
 
+class Stationery:
+    def __init__(self, title):
+        self.title = title
+
+    def draw(self):
+        return f'Запуск отрисовки'
+
+
+class Pen(Stationery):
+    def draw(self):
+        return f'Запуск отрисовки {self.title}'
+
+
+class Pencil(Stationery):
+    def draw(self):
+        return f'Запуск отрисовки {self.title}'
+
+
+class Handle(Stationery):
+    def draw(self):
+        return f'Запуск отрисовки {self.title}'
+
+
+pen = Pen(' - ручка')
+print(pen.draw())
+pencil = Pencil(' - карандаш')
+print(pencil.draw())
+handle = Handle(' - маркер')
+print(handle.draw())
