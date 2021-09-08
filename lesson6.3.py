8fbd986480c525913c99099632fa6ec4ed8ad79d
Vitaly <vit050587@mail.ru>
2021-09-08 09:23:51 +0300

    Добавлены решения GB к 6 уроку (3)

--- lesson6.3.py
+++ lesson6.3.py
@@ -32,3 +32,26 @@ user = Position("Анатолий", "Сульянов", "BOSS", 50000, 1250)
 print(user.get_full_name())
 print(user.position)
 print(user.get_total_income())
+
+#-----------------------решение GB------------------------
+
+class Worker:
+    def __init__(self, name, surname, position, wage, bonus):
+        self.name = name
+        self.surname = surname
+        self.position = position
+        self._income = {"profit": wage, "bonus": bonus}
+
+
+class Position(Worker):
+    def get_full_name(self):
+        return f"{self.name} {self.surname}"
+
+    def get_full_profit(self):
+        return f"{sum(self._income.values())}"
+
+
+meneger = Position("Dorian", "Grey", "СEO", 500000, 125000)
+print(meneger.get_full_name())
+print(meneger.position)
+print(meneger.get_full_profit())
