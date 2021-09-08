b0b5ade5b579526ce1ea26dd4cfc4f3c226a5f69
Vitaly <vit050587@mail.ru>
2021-09-08 09:26:12 +0300

    Добавлены решения GB к 6 уроку (5)

--- lesson6.5.py
+++ lesson6.5.py
@@ -35,3 +35,39 @@ pencil = Pencil(' - карандаш')
 print(pencil.draw())
 handle = Handle(' - маркер')
 print(handle.draw())
+
+
+#--------------------решения GB---------------------------
+
+class Stationery:
+    def __init__(self, title="Something that can draw"):
+        self.title = title
+
+    def draw(self):
+        print(f"Just start drawing! {self.title}))")
+
+
+class Pen(Stationery):
+    def draw(self):
+        print(f"Start drawing with {self.title} pen!")
+
+
+class Pencil(Stationery):
+    def draw(self):
+        print(f"Start drawing with {self.title} pencil!")
+
+
+class Marker(Stationery):
+    def draw(self):
+        print(f"Start drawing with {self.title} marker!")
+
+
+stat = Stationery()
+pen = Pen("Parker")
+pencil = Pencil("Faber-Castell")
+marker = Marker("COPIC")
+
+office_supplies = [stat, pen, pencil, marker]
+
+for i in office_supplies:
+    i.draw()
