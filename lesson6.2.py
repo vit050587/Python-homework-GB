1e8dc8da48fe8d8fe455a64288ca3ea5e754397b
Vitaly <vit050587@mail.ru>
2021-09-08 09:22:47 +0300

    Добавлены решения GB к 6 уроку (2)

--- lesson6.2.py
+++ lesson6.2.py
@@ -22,3 +22,42 @@ class Road:
 
 r = Road(5000, 20)
 r.mass()
+
+
+#------------------------решения GB------------------------------
+
+
+class Road:
+    def __init__(self, length, width):
+        self._length = length
+        self._width = width
+
+    def get_full_profit(self, weight=25, thickness=5):
+        return f"{self._length} м * {self._width} м * {weight} кг * {thickness} см =" \
+               f" {(self._length * self._width * weight * thickness) / 1000} т"
+
+
+road_1 = Road(5000, 20)
+print(road_1.get_full_profit())
+
+
+#  ------------------------------------------- вариант решения ---------------------------------------------------------
+
+
+class Road:
+    def __init__(self, le, wi):
+        self._length = le
+        self._width = wi
+
+    def calc(self, weight=25, thickness=5):
+        print(f"Масса асфальта - {self._length * self._width * weight * thickness / 1000} тонн")
+
+
+def main():
+    while True:
+        try:
+            road_1 = Road(int(input("Enter width of road in m: ")), int(input("Enter lenght of road in m: ")))
+            road_1.calc()
+            break
+        except ValueError:
+            print("Only integer!")
