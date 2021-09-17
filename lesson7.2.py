25713344615d7903051729641c0c287727416274
Vitaly <vit050587@mail.ru>
2021-09-17 22:51:02 +0300

    Добавлены ДЗ к 7 уроку (1)

--- lesson7.2.py
+++ lesson7.2.py
@@ -1,11 +1,55 @@
-#2. Реализовать проект расчета суммарного расхода ткани на производство одежды.
+# 2. Реализовать проект расчета суммарного расхода ткани на производство одежды.
 # Основная сущность (класс) этого проекта — одежда, которая может иметь определенное название.
 # К типам одежды в этом проекте относятся пальто и костюм.
 # У этих типов одежды существуют параметры: размер (для пальто) и рост (для костюма).
 # Это могут быть обычные числа: V и H, соответственно.
-#Для определения расхода ткани по каждому типу одежды использовать формулы: для пальто (V/6.5 + 0.5),
+# Для определения расхода ткани по каждому типу одежды использовать формулы: для пальто (V/6.5 + 0.5),
 # для костюма (2 * H + 0.3). Проверить работу этих методов на реальных данных.
-#Реализовать общий подсчет расхода ткани.
+# Реализовать общий подсчет расхода ткани.
 # Проверить на практике полученные на этом уроке знания: реализовать абстрактные классы для основных классов проекта,
 # проверить на практике работу декоратора @property.
 
+from abc import ABC, abstractmethod
+
+
+class Clothes(ABC):
+    square = 0
+
+    def __init__(self, param):
+        self.param = param
+
+    @property
+    @abstractmethod
+    def consumption(self):
+        pass
+
+    def __add__(self, other):
+        Clothes.square += self.consumption + other.consumption
+        return Costume(0)
+
+    def __str__(self):
+        sgr = Clothes.square
+        Clothes.square = 0
+        return f'{sgr}'
+
+
+class Coat(Clothes):
+    @property
+    def consumption(self):
+        return round((self.param / 6.5) + 0.5)
+
+
+class Costume(Clothes):
+    @property
+    def consumption(self):
+        return round((2 * self.param + 0.3) / 100)
+
+
+coat = Coat(40)
+costume = Costume(180)
+print('Для пальто нужно:')
+print(coat.consumption)
+print('Для костюма нужно:')
+print(costume.consumption)
+print('Общее кол-во ткани:')
+print(coat + costume + coat)
--- lesson7.3.py
+++ lesson7.3.py
@@ -22,3 +22,44 @@
 # Тогда метод make_order() вернет строку: *****\n*****\n**.
 #Или, количество ячеек клетки равняется 15, количество ячеек в ряду — 5.
 # Тогда метод make_order() вернет строку: *****\n*****\n*****.
+
+class Cell:
+    def __init__(self, quantity):
+        self.quantity = int(quantity)
+
+    def __str__(self):
+        return f'Результат операции - {self.quantity * "|"}'
+
+    def __add__(self, other):
+        self.result = Cell(self.quantity + other.quantity)
+        return Cell(self.quantity + other.quantity)
+
+    def __sub__(self, other):
+        return self.quantity - other.quantity if (self.quantity - other.quantity) > 0 else print('Отрицательно!')
+
+        return Cell(int(self.quantity - other.quantity))
+
+    def __mul__(self, other):
+        self.result = Cell(int(self.quantity * other.quantity))
+        return Cell(int(self.quantity * other.quantity))
+
+    def __truediv__(self, other):
+        self.result = Cell(round(self.quantity // other.quantity))
+        return Cell(round(self.quantity // other.quantity))
+
+
+    def make_order(self, cells_in_row):
+        row = ''
+        for i in range(int(self.quantity / cells_in_row)):
+            row += f'{"-" * cells_in_row}\n'
+        row += f'{"-" * (self.quantity % cells_in_row)}'
+        return row
+
+cells1 = Cell(33)
+cells2 = Cell(9)
+print(cells1)
+print(cells1 + cells2)
+print(cells1 - cells2)
+print(cells2.make_order(5))
+print(cells1.make_order(10))
+print(cells1 / cells2)
